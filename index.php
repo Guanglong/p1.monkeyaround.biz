a86488a1a8d3c90e8b18f9338a43184fa548d63b
guang.long <gwong.long.2013@gmail.com>
2013-09-14 12:21:48 -0400

    add real content

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-Welcome to P1
+    <!DOCTYPE html>  
+    <head>  
+      <meta charset="utf-8">  
+      <title>P1.MonkeyAround.biz</title>  
+      <meta name="description" content="Welcome to P1.MonkeyAround.biz."> 
+      <meta name="author" content="guanglong-harvard extension school student">      
+    </head>  
+      
+    <body>  
+        <div id="mainBody">  
+            <header>  
+                <h1>Welcome to my P1.MonkeyAround.biz</h1>  
+            </header>  
+              
+            <div id="mainContent" >  
+                <section id="name">  
+                   <p>Guang Long</p>  
+                </section>  
+                  
+                <section id="bio">  
+                    <p>I am an software developer with a higher education institute. I develop web applications using various   technologies, primarily Java, JavaScript, Oracle, Weblogic. Prior to that, I was a software engineer for a private software companay for MedMal practices and Workers' Compensation Insurance, I worked on migrating the software from the client/server version to the web. I also worked on direct client implementations to customize software for various insurance carriers. </p>
+                </section> 
+
+                <section id="editor">  
+                    <p>GEDIT is the editor I use for this course. I have used a few different editors, for example: eClipse, Intellij IDEA, UltraEditor. But since GEDIT comes with my Linux (Ubunto) installation by default, it makes easy for me to use it.</p>  
+                </section> 
+                <section id="conclusion">
+                  <p>  Already after the second class, I felt that I learned a lot from the course. look forward to DWA15 class! </P>  
+                </section>
+            </div>  
+              
+            <footer>  
+                <p>&nbsp;</p>  
+            </footer>  
+        </div>  
+      
+    </body>  
+    </html>  
